e0333daacc4298d4cdd2ee08a9fe38daea78f6b5
hahnlee <hanlee.dev@gmail.com>
2022-09-29 20:46:21 +0900

    refactor(hwp): Integer 제너릭 지원

--- crates/hwp/src/hwp/utils/bits.rs
+++ crates/hwp/src/hwp/utils/bits.rs
@@ -1,16 +1,27 @@
-pub fn get_value(bits: u32, start: u32, end: u32) -> u32 {
+use std::ops::{AddAssign, BitAnd, Shl, ShlAssign, Shr};
+
+use num::{one, zero, Integer};
+
+pub fn get_value<T: Integer>(bits: T, start: T, end: T) -> T
+where
+    T: Shl<Output = T> + Shr<Output = T> + ShlAssign + AddAssign + BitAnd<Output = T> + Copy,
+    std::ops::Range<T>: IntoIterator,
+{
     let target = bits >> start;
 
-    let mut mask = 0;
+    let mut mask = zero::<T>();
     for _ in start..(end - start) {
-        mask <<= 1;
-        mask += 1;
+        mask <<= one::<T>();
+        mask += one::<T>();
     }
 
     return target & mask;
 }
 
-pub fn get_flag(bits: u32, position: u32) -> bool {
-    let mask = 1 << position;
+pub fn get_flag<T: Integer>(bits: T, position: T) -> bool
+where
+    T: Shl<Output = T> + BitAnd<Output = T> + Copy,
+{
+    let mask = one::<T>() << position;
     return (bits & mask) == mask;
 }
