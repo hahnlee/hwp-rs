2a284004c48a512fb63a95cf1fc992eedd774676
hahnlee <hanlee.dev@gmail.com>
2022-10-03 21:48:11 +0900

    feat(hwp): 파라그래프 누락된 데이터 파싱

---
+++ crates/hwp/src/hwp/paragraph/char_shape.rs
@@ -0,0 +1,23 @@
+use std::io::Read;
+
+use byteorder::{LittleEndian, ReadBytesExt};
+
+#[derive(Debug)]
+pub struct CharShape {
+    /// 글자 모양이 바뀌는 시작 위치
+    pub start_position: u32,
+    /// 글자 모양 ID
+    pub shape_id: u32,
+}
+
+impl CharShape {
+    pub fn from_reader<T: Read>(reader: &mut T) -> Self {
+        let start_position = reader.read_u32::<LittleEndian>().unwrap();
+        let shape_id = reader.read_u32::<LittleEndian>().unwrap();
+
+        Self {
+            start_position,
+            shape_id,
+        }
+    }
+}
--- crates/hwp/tests/integration/naver_documents/mod.rs
+++ crates/hwp/tests/integration/naver_documents/mod.rs
@@ -21,3 +21,22 @@ fn check_annual_report() {
 
     // TODO: (@hahnlee) 정보 채우기
 }
+
+#[test]
+fn check_work_report() {
+    let path = get_tests_path("integration/naver_documents/files/work_report.hwp");
+    let file = fs::read(path).unwrap();
+
+    let hwp = HWP::from_bytes(file);
+
+    assert_eq!(hwp.header.version.to_string(), "5.0.2.4");
+    assert_eq!(hwp.header.flags.compressed, true);
+    assert_eq!(hwp.header.flags.distributed, false);
+
+    assert_eq!(hwp.header.license.ccl, false);
+    assert_eq!(hwp.header.license.replication_restrictions, false);
+
+    assert_eq!(hwp.body_texts.sections.len(), 1);
+
+    // TODO: (@hahnlee) 정보 채우기
+}
