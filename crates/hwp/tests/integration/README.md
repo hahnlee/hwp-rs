691bd0351e76aca83a700973d7badda0a05146bb
hahnlee <hanlee.dev@gmail.com>
2022-10-03 19:11:17 +0900

    test(hwp): 한컴 문서 테스트

--- crates/hwp/tests/integration/README.md
+++ crates/hwp/tests/integration/README.md
@@ -1,2 +1,12 @@
 # integration
 실제 한글 파일을 읽고 테스트합니다.
+
+## naver_documents
+네이버에서 공개한 [한글 문서 서식](https://hangeul.naver.com/document)을 테스트합니다.
+
+## hancom
+[한글 파일형식 문서](https://www.hancom.com/etc/hwpDownload.do)를 테스트 합니다.
+배포용 문서 읽기를 테스트 합니다.
+
+## project
+hwp 프로젝트에서 직접 만든 파일로, 여러 상태의 hwp를 커버하기 위해 사용 합니다.
---
+++ crates/hwp/tests/integration/hancom/mod.rs
@@ -0,0 +1,45 @@
+use hwp::HWP;
+use std::fs;
+
+use crate::utils::get_tests_path;
+
+#[test]
+fn check_hwp_document_format() {
+    let path = get_tests_path("integration/hancom/files/한글문서파일형식_5.0_revision1.3.hwp");
+    let file = fs::read(path).unwrap();
+
+    let hwp = HWP::from_bytes(file);
+
+    assert_eq!(hwp.header.version.to_string(), "5.1.0.1");
+    assert_eq!(hwp.header.flags.compressed, true);
+    assert_eq!(hwp.header.flags.distributed, true);
+
+    assert_eq!(hwp.header.license.ccl, false);
+    assert_eq!(hwp.header.license.replication_restrictions, false);
+
+    // 배포용 문서는 ViewTexts가 있어야 한다
+    assert_eq!(hwp.view_texts.is_some(), true);
+
+    // TODO: (@hahnlee) 정보 채우기
+}
+
+
+#[test]
+fn check_hwp_equation_format() {
+    let path = get_tests_path("integration/hancom/files/한글문서파일형식_수식_revision1.3.hwp");
+    let file = fs::read(path).unwrap();
+
+    let hwp = HWP::from_bytes(file);
+
+    assert_eq!(hwp.header.version.to_string(), "5.1.0.1");
+    assert_eq!(hwp.header.flags.compressed, true);
+    assert_eq!(hwp.header.flags.distributed, true);
+
+    assert_eq!(hwp.header.license.ccl, false);
+    assert_eq!(hwp.header.license.replication_restrictions, false);
+
+    // 배포용 문서는 ViewTexts가 있어야 한다
+    assert_eq!(hwp.view_texts.is_some(), true);
+
+    // TODO: (@hahnlee) 정보 채우기
+}
