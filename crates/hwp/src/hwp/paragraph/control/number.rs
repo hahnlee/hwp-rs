4bc4b7e8c737b6e516eae6ee6cce9a08adcd00c3
hahnlee <hanlee.dev@gmail.com>
2022-10-28 18:56:19 +0900

    feat(hwp): 자동 숫자 포메팅

--- crates/hwp/src/hwp/paragraph/control/number.rs
+++ crates/hwp/src/hwp/paragraph/control/number.rs
@@ -7,9 +7,7 @@ use crate::hwp::{
     utils::bits::{get_flag, get_value_range},
 };
 
-const TEN_HEAVENLY_STEMS_KR: [char; 10] =
-    ['갑', '을', '병', '정', '무', '기', '경', '신', '임', '계'];
-const TEN_HEAVENLY_STEMS: [char; 10] = ['甲', '乙', '丙', '丁', '戊', '己', '庚', '辛', '壬', '癸'];
+use super::section::{NumberShape, format_number_shape};
 
 /// 번호 종류
 #[repr(u32)]
@@ -37,7 +35,7 @@ pub struct AutoNumber {
     /// 번호 종류
     pub kind: NumberKind,
     // 번호 모양
-    pub shape: u32,
+    pub number_shape: NumberShape,
     /// 각주에서만 사용된다.
     /// 각주 내용 중 번호 코드의 모양을 위 첨자 형식으로 할지 여부.
     pub superscript: bool,
@@ -58,7 +56,7 @@ impl AutoNumber {
 
         let properties = reader.read_u32::<LittleEndian>().unwrap();
         let kind = NumberKind::from_u32(get_value_range(properties, 0, 3)).unwrap();
-        let shape = get_value_range(properties, 4, 11);
+        let number_shape = NumberShape::from_u32(get_value_range(properties, 4, 11)).unwrap();
         let superscript = get_flag(properties, 12);
 
         let number = reader.read_u16::<LittleEndian>().unwrap();
@@ -72,7 +70,7 @@ impl AutoNumber {
         Self {
             ctrl_id,
             kind,
-            shape,
+            number_shape,
             superscript,
             number,
             user_char,
@@ -82,16 +80,7 @@ impl AutoNumber {
     }
 
     pub fn to_string(&self) -> String {
-        // TODO: (@hahnlee) 다른 방법도 만들어주기
-        match self.shape {
-            0 => format!("{}", self.number),
-            15 => format!(
-                "{}",
-                TEN_HEAVENLY_STEMS_KR[((self.number - 1) % 10) as usize]
-            ),
-            16 => format!("{}", TEN_HEAVENLY_STEMS[((self.number - 1) % 10) as usize]),
-            _ => format!(""),
-        }
+        format_number_shape(&self.number_shape, self.number)
     }
 }
 
