88f7b738d0382edff1178fb2a10c554b94d9378d
hahnlee <hanlee.dev@gmail.com>
2022-11-11 17:46:22 +0900

    feat(hwp): 원 파싱

--- crates/hwp/src/hwp/paragraph/control/shape_object/ellipse.rs
+++ crates/hwp/src/hwp/paragraph/control/shape_object/ellipse.rs
@@ -1,11 +1,18 @@
+use byteorder::{LittleEndian, ReadBytesExt};
+
 use crate::hwp::{
     paragraph::control::{
-        common_properties::CommonProperties, draw_text::DrawText,
+        common_properties::CommonProperties,
+        draw_text::DrawText,
         element_properties::ElementProperties,
+        shape_object::{arc::ArcKind, picture::Point},
     },
     record::{tags::BodyTextRecord, Record, RecordCursor},
+    utils::bits::{get_flag, get_value_range},
     version::Version,
 };
+
+use num::FromPrimitive;
 
 /// 타원
 #[derive(Debug, Clone)]
@@ -43,7 +50,29 @@ impl ShapeEllipseControl {
 }
 
 #[derive(Debug, Clone)]
-pub struct EllipseRecord {}
+pub struct EllipseRecord {
+    /// 호(ARC)로 바뀌었을 때, interval을 다시 계산해야 할 필요가 있는지 여부
+    /// (interval - 원 위에 존재하는 두 점 사이의 거리)
+    pub interval_dirty: bool,
+    /// 호(ARC)로 바뀌었는지 여부
+    pub has_arc_property: bool,
+    /// 호(ARC)의 종류
+    pub arc_kind: ArcKind,
+    /// 중심 좌표
+    pub center: Point,
+    /// 제1축 좌표
+    pub axis_1: Point,
+    /// 제2축 좌표
+    pub axis_2: Point,
+    /// 시작지점 1 좌표
+    pub start_1: Point,
+    /// 끝지점 1 좌표
+    pub end_1: Point,
+    /// 시작지점 2 좌표
+    pub start_2: Point,
+    /// 끝지점 2 좌표
+    pub end_2: Point,
+}
 
 impl EllipseRecord {
     pub fn from_record_cursor(cursor: &mut RecordCursor) -> Self {
@@ -53,7 +82,34 @@ impl EllipseRecord {
             BodyTextRecord::HWPTAG_SHAPE_COMPONENT_ELLIPSE as u32
         );
 
-        // TODO: (@hahnlee)
-        Self {}
+        let mut reader = record.get_data_reader();
+
+        let attribute = reader.read_u32::<LittleEndian>().unwrap();
+        let interval_dirty = get_flag(attribute, 0);
+        let has_arc_property = get_flag(attribute, 1);
+        let arc_kind = ArcKind::from_u32(get_value_range(attribute, 2, 9)).unwrap();
+
+        let center = Point::from_reader(&mut reader);
+        let axis_1 = Point::from_reader(&mut reader);
+        let axis_2 = Point::from_reader(&mut reader);
+        let start_1 = Point::from_reader(&mut reader);
+        let end_1 = Point::from_reader(&mut reader);
+        let start_2 = Point::from_reader(&mut reader);
+        let end_2 = Point::from_reader(&mut reader);
+
+        assert_eq!(record.size as u64, reader.position());
+
+        Self {
+            interval_dirty,
+            has_arc_property,
+            arc_kind,
+            center,
+            axis_1,
+            axis_2,
+            start_1,
+            end_1,
+            start_2,
+            end_2,
+        }
     }
 }
